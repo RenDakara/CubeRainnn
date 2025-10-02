a9cb025f063c668bb899ddc12dd4da01a4d88369
RenDakara <bobo1331@yahoo.com>
2025-10-02 16:12:15 +0300

    Update ColorChanger.cs

--- Assets/Scripts/ColorChanger.cs
+++ Assets/Scripts/ColorChanger.cs
@@ -1,12 +1,10 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class ColorChanger : MonoBehaviour
 {
     public void ChangeColor(GameObject obj)
     {
-        Color color = new Color(UnityEngine.Random.value, UnityEngine.Random.value, UnityEngine.Random.value);
+        Color color = Random.ColorHSV();
         Renderer renderer = obj.GetComponent<Renderer>();
         renderer.material.color = color;
     }
